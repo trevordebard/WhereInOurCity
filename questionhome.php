1d9c10df31351642afa766c4b5db778b8f1e60a8
Trevor DeBardeleben <trevordebard@gmail.com>
2017-11-13 12:17:18 -0600

    build layout for comments and posting comment (Matt and Trevor pair program)

--- questionhome.php
+++ questionhome.php
@@ -9,7 +9,7 @@
       color: #242424;
     }
     #question-description {
-      font-size: 18px;
+      font-size: 17px;
       margin-bottom: 10px;
       color: #242424;
     }
@@ -20,12 +20,64 @@
       width: 60%;
       margin-left: 7%;
       margin-top: 2%;
-
     }
+    #post-comment-container {
+      margin-bottom: 15px;
+      display: block;
+    }
+    #comment-dropdown {
+      text-decoration: none;
+      display: block;
+      color: black;
+      max-width: 87px;
+    }
+    #comment-dropdown:hover {
+      color: #247ba0;
+    }
+    #comment-textarea {
+      resize: vertical;
+      margin: 10px;
+      width: 85%;
+    }
+    #commment-form-containter {
+      background-color: #F3F7FF;
+      width: 100%;
+    }
+    #glyph-up {
+      display: none;
+    }
+    #submit-comment {
+      float: right;
+      margin-right: 15%;
+    }
+    .display-none {
+      display: none;
+    }
+    .display-inline-block {
+      display: inline-block;
+    }
+
+    .comment-lgi:hover {
+      border-color: #247ba0;
+      border: 2px solid #247ba0;
+    }
+
     .comment-lgi {
+      padding: 10px;
+      margin-bottom: 5px;
+    }
+
+    .sidebar-container{
+      width: 33%;
+      position: relative;
+      text-align: center;
+      float: right;
     }
     hr {
-      border: .84px solid #247ba0;
+      border: .84px solid #BCBBBB;
+    }
+    p {
+      font-size: 16px;
     }
     .wrap-text {
       overflow-wrap: break-word;
@@ -34,6 +86,7 @@
   </style>
 </head>
 <body>
+
   <?php
     include("templates/main-navbar.html");
     include("templates/login-modal.html");
@@ -43,29 +96,50 @@
     $query = "SELECT posts_question, posts_description FROM T_Posts WHERE posts_id=".$_GET['id']."";
     $results = mysqli_query($conn, $query);
     $row = mysqli_fetch_assoc($results);
+    echo '<div class="sidebar-container"></div>';
     echo '<div id="quest-comment-container"><div id="question-container"><h2 id="question" class="wrap-text">'.$row['posts_question'].'</h2>';
     echo '<p id="question-description" class="wrap-text">'.$row['posts_description'].'</p><hr></div>';
   ?>
+  <div>
+  </div>
+    <div id="post-comment-container">
+      <a id="comment-dropdown" href="#">
+        <span id="glyph-down" class="glyphicon glyphicon-menu-down" aria-hidden="true"></span>
+        <span id="glyph-up" class="glyphicon glyphicon-menu-up" aria-hidden="true"></span>
+        Comment</a>
+      <div id="commment-form-containter" class="display-none">
+        <form>
+          <textarea id="comment-textarea" class="form-control"></textarea>
+          <input id="submit-comment" type="button" class="btn" value="Post comment">
+        </form>
+      </div>
+    </div>
+
     <a href="#" class="list-group-item  list-group-item-action comment-lgi wrap-text">
       <div>
         <h5>username</h5>
-        <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h3>
       </div>
     </a>
     <a href="#" class="list-group-item list-group-item-action comment-lgi wrap-text">
       <div>
         <h5>username</h5>
-        <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h3>
       </div>
     </a>
     <a href="#" class="list-group-item list-group-item-action comment-lgi wrap-text">
       <div>
         <h5>username</h5>
-        <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h3>
       </div>
     </a>
   </div>
-
-
+  <script>
+    $("#comment-dropdown").click(function() {
+      $("#commment-form-containter").toggleClass('display-inline-block display-none');
+      $("#glyph-up").toggle();
+      $("#glyph-down").toggle();
+    })
+  </script>
 </body>
 </html>
