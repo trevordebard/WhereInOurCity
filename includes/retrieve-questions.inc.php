8290f8ccd24214338fe2ed42f968bad7d42b758c
Trevor DeBardeleben <trevordebard@gmail.com>
2017-11-26 13:34:18 -0600

    One last fix

--- includes/retrieve-questions.inc.php
+++ includes/retrieve-questions.inc.php
@@ -1,31 +1,21 @@
 <?php
-  include 'dbh.php';
-  $query = "SELECT * FROM T_Posts ORDER BY posts_id DESC";
-  $results = mysqli_query($conn,$query);
-  $resultCheck = mysqli_num_rows($results);
-  if($resultCheck < 1){
-    echo 'helo';
-  }
-  else {
-    while ($row = mysqli_fetch_assoc($results)) {
-      echo
-        '<a href="questionhome.php?id='.$row['posts_id'].'" class="list-group-item  list-group-item-action flex-column align-items-start wrap-text">
-          <div class="d-flex w-100 justify-content-between">
-            <h2>'.$row['posts_question'].'</h2>
-            <p>'.$row['posts_description'].'</p>
-            <button class="btn">Helpful?</button>
-          </div>
-        </a>';
+  if (isset($_POST['fillPage'])) {
+    include 'dbh.php';
+    $start = $conn->real_escape_string($_POST['start']);
+    $limit = $conn->real_escape_string($_POST['questionsPerLoad']);
+
+    $query = "SELECT * FROM T_Posts ORDER BY posts_id DESC LIMIT $start, $limit";
+    $results = mysqli_query($conn,$query);
+    $resultCheck = mysqli_num_rows($results);
+    if($resultCheck < 1){
+      exit('reachedMax');
     }
-<<<<<<< HEAD
-  }
-=======
     else {
       $response = "";
       while ($row = mysqli_fetch_assoc($results)) {
         //The .= concatonates the string to itself
         $response .= '
-          <a href="questionhome.php?id='.$row['posts_id'].'" class="list-group-item  list-group-item-action flex-column align-items-start wrap-text">
+          <a href="questionhome.php?id='.$row['posts_id'].'" class="list-group-item list-group-item-action flex-column align-items-start wrap-text">
             <div class="d-flex w-100 justify-content-between">
               <h2>'.$row['posts_question'].'</h2>
               <p>'.$row['posts_description'].'</p>
@@ -37,5 +27,4 @@
       exit($response);
     }
 }
->>>>>>> origin/master
 ?>
