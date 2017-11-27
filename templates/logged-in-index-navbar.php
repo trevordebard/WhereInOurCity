2a87ed587a52d5dab9793c3393649316776482aa
Logan Serio <Loganserio@gmail.com>
2017-11-26 23:14:08 -0600

    Fix fatal error with voting system

--- templates/logged-in-index-navbar.php
+++ templates/logged-in-index-navbar.php
@@ -14,15 +14,20 @@ echo '<nav class="navbar navbar-default">
           <ul role="menu" class="dropdown-menu" aria-labelledby="drop1">
             <li role="presentation"><a href="profilePage.php" role="menuitem">Profile</a></li>
             <li role="presentation"><a href="#" role="menuitem">Notifications</a></li>
-            <li role="presentation"><a href="#" role="menuitem">Log Out</a></li>
+            <li role="presentation"><a href="#" id="logout" role="menuitem">Log Out</a></li>
           </ul>
       </li>
       <li><a href="#ourMissionHeader" id="ourMissionBtn" class="underlineAnimate navbarText">Our Mission</a></li>
       <li><a href="#" id="contactUsBtn" class="underlineAnimate navbarText" data-toggle="modal" data-target="#contact-modal">Contact Us</a></li>
       <li><form action="includes/logout.inc.php" method="POST">
-        <li><button type="submit" name="logoutBtn">Log Out</button></li>
+        <li><button style="display:none" type="submit" id="logoutBtn" name="logoutBtn">Log Out</button></li>
       </form></li>
     </ul>
   </div>
 </nav>';
 ?>
+<script>
+$("#logout").click(function() {
+  $("#logoutBtn").trigger("click");
+});
+</script>
