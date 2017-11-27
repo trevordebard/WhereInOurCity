bce0c98fe4f81cdd3df8c5e697e320f5b11a335e
colegluth <colegluth@gmail.com>
2017-11-27 03:03:24 -0600

    Add comments.

--- index.php
+++ index.php
@@ -11,20 +11,20 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <style>
     #btn-choose-location {
-        border-color: #247BA0;
-        max-width:600px;
-        width:100%;
-        background-color: #247BA0;
-        font-family: helvetica, sans-serif;
+        border-color: #247BA0; //Sets the color of an elements four borders.
+        max-width:600px; //Esablishes the max-width of the choose location button.
+        width:100%; //Sets the default width to 100%.
+        background-color: #247BA0; //Set the background color for the button.
+        font-family: helvetica, sans-serif; //Defines the default fonts for the button.
     }
     #btn-choose-location:hover {
-      background-color: #0A678F;
+      background-color: #0A678F; //Alters the color of the choose location button when the user hovers over it.
     }
-    #cont-search-location{
-      width:75%;
-      max-width:600px;
-      margin-left:50%;
-      transform: translateX(-50%);
+    #cont-search-location{ //Creates a container for selection a location.
+      width:75%; //Sets the default width to 75% of the container.
+      max-width:600px;  //Establishes the max-width of the container as 600 pixels.
+      margin-left:50%; //Sets the left-margin of the search location container.
+      transform: translateX(-50%); //Shifts the container left by 50%. This in combination with margin-left 50% centers the container.
     }
     /*****Underline Animation****/
     .nav a {
@@ -39,32 +39,32 @@
 
     /*************************/
     .space{
-      margin-top: 460px;
+      margin-top: 460px; //Adds a margin of 460px to the top of the Our Mission information.
     }
     .textCentered{
-      text-align:center;
+      text-align:center; //Centers the Our Mission text header.
     }
     #ourMissionHR{
-      width:75%;
-      border-color:#247BA0;
+      width:75%; //Sets the width for details for Our Mission to 75% of the screen's width.
+      border-color:#247BA0; //Sets the border-color of the Our Mission header.
     }
     .ourMissionText{
-      margin:auto;
-      max-width:75%;
+      margin:auto; //Sets the margin of the text for Our Mission to its default value.
+      max-width:75%; //Establishs the maximum width of the Our Mission text.
     }
     #ourMissionHeader{
-        text-decoration: none;
-        color:#247BA0;
+        text-decoration: none; //Defines that the text will not have any decoration.
+        color:#247BA0; //Sets the primary color of the Our Mission header.
     }
     #message-text {
       resize: vertical;
     }
     .navbarText {
-      font-size: 15px;
-      font-weight: bold;
-      font-family: helvetica, sans-serif;
-      postion:relative;
-      top:-8px;
+      font-size: 15px; //Sets the navbar font size to 15 pixels.
+      font-weight: bold; //Makes the navbar text bold.
+      font-family: helvetica, sans-serif; //Defines the font that the navbar text can choose from.
+      postion:relative; //The navbar's positioning is relative to its normal position.
+      top:-8px; //Sets the top edge of the navbar as 8 pixels higher than default.
     }
     .nav.navbar-nav.navbar-right li a {
       color: #464646;
@@ -74,8 +74,8 @@
       color: #000000;
     }
     .navbar-default{
-      background-color: white;
-      border:none;
+      background-color: white; //The navbar has a while background color.
+      border:none; //The navbar does not have visible borders.
     }
     </style>
   </head>
@@ -86,7 +86,7 @@
       include("templates/login-modal.html");
       include("templates/contact-us-modal.html");
 
-      if(isset($_SESSION['u_username'])){ //If the session variable u_username is set
+      if(isset($_SESSION['u_username'])){ //If the session variable u_username is
         include("templates/logged-in-index-navbar.php"); //The users username will be displayed in the navbar
       }
       else{
@@ -110,13 +110,13 @@
     <div class="modal fade" id="myModal" role="dialog">
       <div class="modal-dialog">
         <div class="modal-content">
-          <div class="modal-body">
+          <div class="modal-body"> <!--Selecting the Baton Rouge button opens up the corresponding cityhome.php-->
             <a href="cityhome.php"><button class="btn btn-primary horizontal-center brBtn">Baton Rouge</button></a>
           </div>
         </div>
       </div>
     </div>
-
+    <!--This block of text shows the development team's mission with the project. Currently it is filled with Lorem Ipsum place holder text.-->
     <div id="ourMissionText">
       <hr id="ourMissionHR" class="space">
       <h2 class="textCentered"><a id="ourMissionHeader">Our Mission</a></h2>
@@ -127,16 +127,16 @@
       <p class="ourMissionText">Nunc pharetra, ex non maximus ultrices, metus erat sollicitudin orci, in scelerisque arcu lacus elementum mauris. Praesent eleifend consequat nisl, quis convallis eros dapibus ut. Fusce quam erat, maximus eget blandit id, posuere vitae ipsum. Vestibulum pharetra mauris vel mauris tincidunt suscipit. Aliquam consequat, diam eu feugiat volutpat, lorem felis vestibulum ex, nec porta dui dolor sit amet augue. Pellentesque sit amet rhoncus leo. Vestibulum tempor vestibulum malesuada. Morbi semper accumsan justo, eu volutpat nisl volutpat eu.
       In hac habitasse platea dictumst. Nam et fermentum dui. Ut nec pretium leo. Fusce ex magna, facilisis eget purus eu, rhoncus venenatis velit. In purus augue, lacinia vel metus id, consequat congue lectus. Etiam hendrerit sed ligula id ullamcorper. Donec interdum iaculis orci, sit amet pharetra enim dapibus ut. Donec luctus tellus nulla, ac maximus tellus ullamcorper non. Phasellus vitae nunc et lacus feugiat placerat. Curabitur blandit fringilla luctus. Sed egestas lectus non orci cursus, a dictum nibh imperdiet. Vestibulum rutrum vestibulum vehicula.
       Suspendisse scelerisque est non elit pretium malesuada. Nulla orci leo, sagittis eu congue et, scelerisque in sem. Quisque posuere urna non arcu vulputate varius. In cursus nibh id leo auctor scelerisque. Phasellus ut ullamcorper est. Aliquam eget nisi id metus efficitur tincidunt. Duis ante nulla, consequat a arcu id, vestibulum semper nisi. Mauris diam nisl, fermentum vel fringilla eu, finibus id tellus. Duis malesuada dui ut dictum venenatis. Cras ultrices et mauris sit amet posuere. </p>
+
     </div>
     <script>
     $(document).ready(function(){
-      $("#ourMissionBtn").click(function() {
-        $('html, body').animate({
-          scrollTop: $("#ourMissionText").offset().top
+      $("#ourMissionBtn").click(function() {//When the Our Mission button is clicked,
+        $('html, body').animate({ //Add animation that scrolls the user down the page.
+          scrollTop: $("#ourMissionText").offset().top//Scrolls from the top to the specified position.
         }, 700);
       });
     });
-
     </script>
   </body>
 </html>
