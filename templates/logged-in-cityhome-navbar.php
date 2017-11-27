e2bf3fad7e5ed712b741ead36ac4b574e4274628
Trevor DeBardeleben <trevordebard@gmail.com>
2017-11-26 20:09:09 -0600

    Fix logout in navbar

--- templates/logged-in-cityhome-navbar.php
+++ templates/logged-in-cityhome-navbar.php
@@ -28,18 +28,22 @@
                 <ul role="menu" class="dropdown-menu" aria-labelledby="drop1">
                   <li role="presentation"><a href="profilePage.php" role="menuitem">Profile</a></li>
                   <li role="presentation"><a href="#" role="menuitem">Notifications</a></li>
-                  <li role="presentation"><a href="#" role="menuitem">Log Out</a></li>
+                  <li role="presentation"><a id="logout" href="#" role="menuitem">Log Out</a></li>
                 </ul>
             </li>
             <li><a href="#" id="contactUsBtn" class="underlineAnimate navbarText" data-toggle="modal" data-target="#contact-modal">Contact Us</a></li>
             <li><form action="includes/logout.inc.php" method="POST">
-              <li><button type="submit" name="logoutBtn">Log Out</button></li>
+              <li><button style="display:none;" id="logoutBtn" type="submit" name="logoutBtn">Log Out</button></li>
             </form></li>
           </ul>
         </div>
       </nav>';
 ?>
 <script>
+  $("#logout").click(function() {
+    $("#logoutBtn").trigger("click");
+  });
+
   $("#inputSearchBar").focus(function() {
     $("#inputSearchBar").animate({
       width: "300px"
