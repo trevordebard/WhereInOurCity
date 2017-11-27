46426851d67ae52983b5e9ceec2b10067bf26003
Trevor DeBardeleben <trevordebard@gmail.com>
2017-11-27 10:39:30 -0600

    Add comments

--- includes/post-comment.inc.php
+++ includes/post-comment.inc.php
@@ -1,20 +1,20 @@
 <?php
 session_start();
 if(isset($_POST['commentSubmitBtn'])) { //checks to make sure commentSubmitBtn brought you to this page
-  if(isset($_SESSION['u_username'])){
-    include_once 'dbh.php';
-    $u_id = $_SESSION[u_id];
-    $post_id = $_SESSION['post_id'];
+  if(isset($_SESSION['u_username'])){ //checks that the user is logged in
+    include_once 'dbh.php'; //include the database helper
+    $u_id = $_SESSION[u_id]; //gets the user id and sets it to $u_id
+    $post_id = $_SESSION['post_id']; //gets the post id and sets it to $post_id
     $comment = mysqli_real_escape_string($conn, $_POST['comment-textarea']); //retrieves comment from textarea and sets to $comment
 
     //insert the user into the db
-    $sql = "INSERT INTO T_Comments (comments_comment, posts_id, users_id) VALUES ('$comment', '$post_id', '$u_id');";
-    mysqli_query($conn, $sql);
-    header("Location: ../questionhome.php?id=$post_id&post=success");
-    exit();
+    $sql = "INSERT INTO T_Comments (comments_comment, posts_id, users_id) VALUES ('$comment', '$post_id', '$u_id');"; //creates sql statement for insert
+    mysqli_query($conn, $sql); //runs the sql statement in the database
+    header("Location: ../questionhome.php?id=$post_id&post=success"); //reloads the questionhome page with the new post
+    exit(); //exits this file
   }
   else{
-    header("Location: ../questionhome.php?pleasesignin");
+    header("Location: ../questionhome.php?pleasesignin"); //relocates to questionhome and indicates user is not signed in
     exit();
   }
 }
--- includes/retrieve-filtered-questions.inc.php
+++ includes/retrieve-filtered-questions.inc.php
@@ -1,21 +1,23 @@
 <?php
-  if (isset($_POST['filter-cateogry'])) {
-    include 'dbh.php';
-    $start = $conn->real_escape_string($_POST['start']);
-    $limit = $conn->real_escape_string($_POST['questionsPerLoad']);
-    $category = $conn->real_escape_string($_POST['category']);
+  if (isset($_POST['filter-cateogry'])) { //makes sure the filter dropdown sent the user to this page
+    include 'dbh.php'; //creates connection to the database
+    $start = $conn->real_escape_string($_POST['start']); //gets the starting location for infinite scroll
+    $limit = $conn->real_escape_string($_POST['questionsPerLoad']); //gets the ending location for infinite scroll
+    $category = $conn->real_escape_string($_POST['category']); //gets the category the user wants to filter
 
-
+    //sql code to pull posts from the database with the requested category
     $query = "SELECT * FROM T_Posts WHERE posts_category='$category' ORDER BY posts_id DESC LIMIT $start, $limit";
-    $results = mysqli_query($conn, $query);
-    $resultCheck = mysqli_num_rows($results);
-    if($resultCheck < 1){
-      exit('reachedMax');
+    $results = mysqli_query($conn, $query); //runs the sql code
+    $resultCheck = mysqli_num_rows($results); //gets the number of rows returned
+    if($resultCheck < 1){ //if there are no rows
+      exit('reachedMax'); //return 'reachedMax', which the ajax call will handle
     }
     else {
-      $response = "";
-      while ($row = mysqli_fetch_assoc($results)) {
+      $response = ""; //set the response to be blank
+      while ($row = mysqli_fetch_assoc($results)) { //iterates through the returned rows
+        //This block of code returns the posts of the given category in a list group item, which will be displayed on cityhome
         //The .= concatonates the string to itself
+        //The $row['YYY'] variables are column YYY of the current row
         $response .= '
           <a href="questionhome.php?id='.$row['posts_id'].'" class="list-group-item list-group-item-action flex-column align-items-start wrap-text">
             <div class="d-flex w-100 justify-content-between">
@@ -26,7 +28,7 @@
           </a>
         ';
       }
-      exit($response);
+      exit($response); //exits with $response for the ajax call
     }
 }
 ?>
